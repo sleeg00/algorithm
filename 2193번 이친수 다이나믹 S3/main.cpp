b84e2895e426f7d778e5889463be0c7915f0df79
sleeg00 <slee000220@gmail.com>
2023-04-01 21:37:27 +0900

    [백준] 2193번 이친수 다이나믹 S3_1

---
+++ 2193번 이친수 다이나믹 S3/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+long a[100];
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    a[0]=1;
+    a[1]=1;
+
+    for(int i=2; i<=n; i++)
+        a[i]=a[i-1]+a[i-2];
+
+    printf("%lld", a[n-1]);
+    return 0;
+}
