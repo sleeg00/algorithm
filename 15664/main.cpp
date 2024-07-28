d8c59deb06d716ebed3ba4629a38df5aa68d955c
sleeg00 <slee000220@gmail.com>
2024-07-28 18:59:25 +0900

    backtraking

--- 15664/main.cpp
+++ 15664/main.cpp
@@ -1,29 +1,26 @@
-
-/*#include <algorithm>
+#include <algorithm>
 #include <iostream>
+#include <vector>
 #include <set>
-#include <math.h>
-
 using namespace std;
 
-int N, length;
+int N, M;
 int number[10];
 bool visit[10];
 int arr[10];
 set<vector<int>> s;
 
 void sol(int count) {
-
-    if (count == length) {
+    if (count == M) {
         vector<int> v;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < count; i++) {
             v.push_back(arr[i]);
         }
         s.insert(v);
         v.clear();
     } else {
         for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
+            if (!visit[i] && count == 0 || !visit[i] && count > 0 && arr[count-1] <= number[i]) {
                 visit[i] = true;
                 arr[count] = number[i];
                 sol(count + 1);
@@ -34,21 +31,15 @@ void sol(int count) {
 }
 
 int main(void) {
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> N >> length;
-
+    cin >> N >> M;
     for (int i = 0; i < N; i++) {
         cin >> number[i];
     }
-
-    sol(0); for(auto itr = s.begin(); itr!=s.end(); itr++) {
-        for(auto i = (*itr).begin(); i!=(*itr).end(); i++) {
-            cout << *i << " ";
+    sol(0);
+    for(auto itr = s.begin(); itr!=s.end(); itr++) {
+        for(auto v = (*itr).begin(); v!=(*itr).end(); v++) {
+            cout << *v << " ";
         }
         cout << endl;
     }
-
 }
- */
