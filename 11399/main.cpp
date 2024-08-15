fcfcc1600e58681f0d5ee553733d54d65e1db23c
sleeg00 <slee000220@gmail.com>
2024-08-15 16:17:13 +0900

    greedy

---
+++ 11399/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int arr[1001];
+
+int main(void) {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+    int hap = 0;
+    int im = 0;
+    for (int i = 0; i < N; i++) {
+        im = im + arr[i];
+        hap += im;
+    }
+    cout << hap;
+}
